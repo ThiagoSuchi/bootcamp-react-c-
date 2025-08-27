95a8d2a59f6477600b738dc4bdc67af841415dc8
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-08-27 19:40:01 -0400

    Operadores de atribuição - Diferença entre Convert e Parse

--- dotNET/Program.cs
+++ dotNET/Program.cs
@@ -1,6 +1,25 @@
 ﻿using bootcamp.Models;
 
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+string apresentacao = "Olá, seja bem vindo.";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80m;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+Console.WriteLine("\n---------------------------------------------\n");
+
 // Target-typed new | Object initilazer
 Pessoa p = new() { Nome = "Thiago", Idade = 22 };
 
 p.Apresentar();
+
