df9d156679616eeb8d6624de6dd7e88667ea738e
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-09-18 13:46:25 -0400

    POO - Herança, polimorfismo, abstração e encapsulamento

---
+++ ExemploPoo/Models/Aluno.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPoo.Models
+{
+    public class Aluno : Pessoa
+    {
+        public double Nota { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá me chamo {Nome} tenho {Idade} anos, e minha nota deste semestre foi {Nota}");
+        }
+    }
+
+}
---
+++ ExemploPoo/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExemploPoo.Models;
+
+//------------ Herança e Polimorfismo ----------------
+Aluno a1 = new()
+{
+    Nome = "Jõao",
+    Idade = 24,
+    Nota = 10
+};
+
+a1.Apresentar();
+
+Professor p1 = new()
+{
+    Nome = "Adailton",
+    Idade = 38,
+    Salario = 3.500M
+};
+
+p1.Apresentar();
+
+
+// //--------- Encapsulamento -------------
+// ContaCorrente c1 = new(222, 1500);
+
+// c1.ExibirSaldo();
+// c1.Sacar(150);
+// c1.ExibirSaldo();
+// c1.Sacar(1400);
+// c1.ExibirSaldo();
+
+
+//--------- Abstração ---------------
+// Pessoa p1 = new()
+// {
+//     Nome = "Dalila",
+//     Idade = 34
+// };
+
+// p1.Apresentar();
