3d498553f3d093b8a7a5da5fa2dc54e98ac10fde
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-09-18 09:34:44 -0400

    Construindo um Sistema de Hospedagem de um Hotel no C#

---
+++ SistemaDeHospedagem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+
+Pessoa p1 = new(nome: "Hóspede 1");
+Pessoa p2 = new(nome: "Hóspede 2");
+Pessoa p3 = new(nome: "Hóspede 3");
+Pessoa p4 = new(nome: "Hóspede 4");
+
+List<Pessoa> hospedes = new() { p1, p2, p3, p4 };
+
+// Cria a suíte
+Suite suite = new(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new(diasReservados: 12);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
